6e0ba468c7ca9b81b5914f1f2c193780206d149a
AsciiMoth <ascii@moth.contact>
2026-04-26 22:56:32 +0400

    chore: remove unused deps

--- bind_windows.go
+++ bind_windows.go
@@ -19,7 +19,7 @@ import (
 	"github.com/asciimoth/gonnect/sockopt"
 	"golang.org/x/sys/windows"
 
-	"golang.zx2c4.com/wireguard/conn/winrio"
+	"github.com/asciimoth/batchudp/winrio"
 )
 
 const (
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.25.5
 require (
 	golang.org/x/net v0.39.0
 	golang.org/x/sys v0.42.0
-	golang.zx2c4.com/wireguard v0.0.0-20250521234502-f333402bd9cb
 )
 
 require (
